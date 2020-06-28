b76fca23d9b01652db4ef90220c0d9cda67a94a8
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-06-28 16:48:47 -0700

    ch 5 fin

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -17,6 +17,7 @@ var auditTask = function (taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl)
 };
 
 var createTask = function (taskText, taskDate, taskList) {
@@ -82,7 +83,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -219,16 +220,22 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    console.log("activate", this),
+    $(this).addClass("dropover"),
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    console.log("deactivate", this),
+    $(this).removeClass("dropover"),
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    console.log("over", event.target),
+    $(event.target).addClass("dropover-active");
   },
-  out: function (event) {
-    console.log("out", event.target);
+  out: function (event) { 
+    console.log("out", event.target),
+    $(event.target).removeClass("dropover-active");
   },
 
 
@@ -274,13 +281,16 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    console.log("drop"),
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function (event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    console.log("out"),
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -297,3 +307,9 @@ $("#remove-tasks").on("click", function () {
 loadTasks();
 
 
+//Refresh interval
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
